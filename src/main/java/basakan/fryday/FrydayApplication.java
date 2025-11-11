140c15e8e03e6f2e340d3b0a90912f31cb958121
gihhyeon <fineblue.kh@gmail.com>
2025-11-11 13:37:03 +0900

    chore: init commit

---
+++ src/main/java/basakan/fryday/FrydayApplication.java
@@ -0,0 +1,13 @@
+package basakan.fryday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrydayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basakan/fryday/FrydayApplicationTests.java
@@ -0,0 +1,13 @@
+package basakan.fryday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
