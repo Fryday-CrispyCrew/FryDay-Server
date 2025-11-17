72684267d5d2790bb6da0db45c9fbbdffcad9452
gihhyeon <fineblue.kh@gmail.com>
2025-11-17 16:31:07 +0900

    API 응답 포맷 지정
    - 성공, 실패 시 응답 포맷 지정
    - ErrorCode enum 추가

---
+++ src/main/java/basakan/fryday/common/ErrorCode.java
@@ -0,0 +1,21 @@
+package basakan.fryday.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메서드입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
+    INVALID_TYPE_VALUE( HttpStatus.BAD_REQUEST, "잘못된 타입의 값입니다."),
+    HANDLE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
+
