fd94d326c555bd737de7cd1f5a67515df7ea34be
gihhyeon <fineblue.kh@gmail.com>
2026-08-14 22:55:00 +0900

    chore: 미사용 쿼리 findRecurrencesToExtend 제거

--- src/main/java/basakan/fryday/repository/todo/RecurrenceRepository.java
+++ src/main/java/basakan/fryday/repository/todo/RecurrenceRepository.java
@@ -1,6 +1,5 @@
 package basakan.fryday.repository.todo;
 
-import basakan.fryday.domain.todo.EndType;
 import basakan.fryday.domain.todo.Recurrence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,9 +9,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface RecurrenceRepository extends JpaRepository<Recurrence, Long> {
-
-    @Query("SELECT r FROM Recurrence r WHERE r.endType = :none AND r.lastGeneratedDate < :thresholdDate AND r.isDeleted = false")
-    List<Recurrence> findRecurrencesToExtend(@Param("none") EndType none, @Param("thresholdDate") LocalDate thresholdDate);
 
     @Query("SELECT r FROM Recurrence r WHERE r.userId = :userId " +
            "AND r.isDeleted = false " +
